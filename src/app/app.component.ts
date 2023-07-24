fafa2848cb7405116616a37bcd7f163c2705defb
Galeyee <galwang173@gmail.com>
2023-07-24 11:08:25 +0600

    set-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DeskDine';
+  results;
+  constructor(
+    private db: AngularFireDatabase,
+    private http: HttpClient
+  ) {
+    this.db.object('users/0').set({ id: 1, name: 'Max Purl', phone: '12344' });
+    const path = 'https://api.github.com/users/hadley/orgs';
+    this.results = http.get(path);
+  }
+
+  onProductCreate(menu: { name: string; price: string }) {
+    console.log(menu);
+    this.http.post(environment.baseUrl + `menu.json`, menu).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyDXdMiO7c8FaL4aVijuzi3fD4hiM9lRAr8',
+    authDomain: 'deskdine.firebaseapp.com',
+    databaseURL: 'https://deskdine-default-rtdb.firebaseio.com',
+    projectId: 'deskdine',
+    storageBucket: 'deskdine.appspot.com',
+    messagingSenderId: '575483001092',
+    appId: '1:575483001092:web:b38c244530b69c212f7851',
+    measurementId: 'G-DWJ2J5WW6N',
+  },
+  BASE_URL: 'https://deskdine-default-rtdb.firebaseio.com/',
+};
