cb2d318cc9612253262f08ac2c61f09774f8b6ed
Galeyee <galwang173@gmail.com>
2023-07-26 16:24:23 +0600

    try 2

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
-  is = false;
-}
+export class FooterComponent {}
