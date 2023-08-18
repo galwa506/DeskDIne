fac24d4b7649543adb298e83e9985f4954d82642
Galwa506 <galey.wangmo@selise.ch>
2023-08-18 12:25:14 +0600

    changes

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -2,8 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Orders } from '../model/order.model';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
-
 @Injectable({
   providedIn: 'root',
 })
