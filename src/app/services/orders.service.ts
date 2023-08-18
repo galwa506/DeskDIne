cfe37e6d1b3f5c6f104338b0a7d41d2e912e9068
Galwa506 <galey.wangmo@selise.ch>
2023-08-18 12:24:49 +0600

    changes

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -3,20 +3,14 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Orders } from '../model/order.model';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+
 @Injectable({
   providedIn: 'root',
 })
 export class OrdersService {
-  constructor(
-    private http: HttpClient,
-    private db: AngularFireDatabase
-  ) {}
+  constructor(private http: HttpClient) {}
 
   postOrders(orders: Orders) {
     return this.http.post(environment.baseUrl + `orders.json`, orders);
-  }
-
-  getItemDetails() {
-    return localStorage.getItem('cart-items');
   }
 }
